205135eeb06d4e8d5a5a38f0e8071b1b99116ff0
aya-younis <ayayounis31@gmail.com>
2020-03-13 13:24:57 -0700

    profile setting and change password design

--- app/src/main/java/com/example/myapplicationg/Settingprofile.java
+++ app/src/main/java/com/example/myapplicationg/Settingprofile.java
@@ -2,13 +2,31 @@ package com.example.myapplicationg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class Settingprofile extends AppCompatActivity {
-
+Button repassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settingprofile);
+        repassword=findViewById(R.id.changepassword);
+        repassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openchangepassword();
+            }
+        });
+
+
+    }
+
+    public void openchangepassword(){
+        Intent ipassword = new Intent(this, ChangePassword.class);
+        startActivity(ipassword);
     }
 }
